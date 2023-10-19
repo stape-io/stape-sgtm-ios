56be49ab7f07336bd378a92be6d21adcc6169886
Deszip <raf.rafiki@gmail.com>
2023-10-20 01:25:16 +0300

    Sample app table

--- StapeSDK/StapeSampleApp/ViewController.swift
+++ StapeSDK/StapeSampleApp/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
 
