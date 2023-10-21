8a2041eb73e3a8153a48447b0eea47fec9a02235
Deszip <raf.rafiki@gmail.com>
2023-10-21 20:26:13 +0300

    Base controllers

---
+++ StapeSDK/StapeSampleApp/ActionsController.swift
@@ -0,0 +1,21 @@
+//
+//  ActionsController.swift
+//  StapeSampleApp
+//
+//  Created by Deszip on 21.10.2023.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ActionsController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
---
+++ StapeSDK/StapeSampleApp/LogsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogsViewController.swift
+//  StapeSampleApp
+//
+//  Created by Deszip on 21.10.2023.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class LogsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
