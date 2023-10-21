6cceb7810e148556b57ccc2fa27cf50943492be4
Deszip <raf.rafiki@gmail.com>
2023-10-22 01:53:25 +0300

    Log streaming

--- StapeSDK/StapeSampleApp/LogsViewController.swift
+++ StapeSDK/StapeSampleApp/LogsViewController.swift
@@ -7,24 +7,35 @@
 //
 
 import UIKit
+import OSLog
+import StapeSDK
 
 class LogsViewController: UIViewController {
-
+    
+    @IBOutlet weak var logTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            do {
+                let logStore = try OSLogStore(scope: .currentProcessIdentifier)
+                let pos = logStore.position(date: Date().addingTimeInterval(-1))
+                print(pos)
+                let entries = try logStore.getEntries(at: pos)
+                    .compactMap { $0 as? OSLogEntryLog }
+                    .filter { $0.subsystem == "com.stape.logger" }
+                    .map { "[\($0.date.formatted())] [\($0.category)] \($0.composedMessage)" }
+                    .joined(separator: "\n")
 
-        // Do any additional setup after loading the view.
+                self.logTextView.text = self.logTextView.text.appending(entries)
+                
+            } catch {
+                //...
+            }
+
+        }
+
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
